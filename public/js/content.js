69afd82cfc710a1885585aab9c0f3bb6c02725ad
Eliseo Esteverena <5142001+eliseoesteverena@users.noreply.github.com>
2024-03-27 21:50:05 -0300

    desarrollo - 0.0.01

---
+++ public/js/content.js
@@ -0,0 +1,3 @@
+const $header = document.querySelector("header");
+
+$header.innerHTML = "<div class=\"int-header\"><a class=\"logo\" href=\"index.html\">&#123;estudio5&#125;<\/a><nav><a href=\"portfolio.html\" style=\"display:none;\">Portfolio<\/a><a href=\"eliseoesteverena.html\">Sobre Mi<\/a><\/nav><\/div>";
